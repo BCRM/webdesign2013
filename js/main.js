0b6f176a9de98a7887676a7346b9f92be73d11a5
Markus Tacker <m@coderbyheart.de>
2013-09-16 19:02:57 +0200

    Load webfonts and slider if required.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
-$(function(){
-    $('.slider').bxSlider(
-        {
-            captions: true,
-            pager: false
+Modernizr.load([
+    {
+        test: Modernizr.fontface,
+        yep: 'http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js',
+        callback: function (url, result, key) {
+            if (!result) return;
+            WebFont.load({
+                google: {
+                    families: ['Ubuntu:400,300,300italic,400italic,700,700italic']
+                }
+            });
         }
-    );
-});
+    },
+    // Slider
+    {
+        test: Modernizr.mq('(min-width: 950px)'),
+        yep: [ 'vendor/bxslider/jquery.bxslider.css', 'css/slider.css', 'vendor/bxslider/jquery.bxslider.js' ],
+        callback: function (url, result, key) {
+            if (!result || key < 2) return;
+            $('div.content-wrap').prepend('<div class="visual"><ul class="slider"><li><img src="img/sessionplanung.jpg" alt="Sessionplanung auf dem BarCamp RheinMain 2012 in Wiesbaden" title="Sessionplanung auf dem BarCamp RheinMain 2012 in Wiesbaden"></li></ul></div>');
+            $('.slider').bxSlider(
+                {
+                    captions: true,
+                    pager: false
+                }
+            );
+        }
+    }
+]);
